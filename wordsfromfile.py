28f3cd0bc536b2d3736c2c7cc442b922d62daa5d
mandularahul <71830393+mandularahul@users.noreply.github.com>
2021-04-06 19:15:13 +0530

    Add files via upload

---
+++ wordsfromfile.py
@@ -0,0 +1,11 @@
+def countwordsfromfile():
+    filename=input("enter the file name ")
+    numberofwords=0
+    file=open(filename,'r')
+    for line in file:
+        words=line.split()
+        numberofwords=numberofwords+len(words)
+    print("number of words")
+    print(numberofwords)
+
+countwordsfromfile()
